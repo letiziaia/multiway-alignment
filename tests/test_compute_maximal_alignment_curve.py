b979daecd1d3ab4883942040179c1295c792a7b4
letiziaia <letizia852@gmail.com>
2023-02-01 17:01:58 +0200

    test

---
+++ tests/test_compute_maximal_alignment_curve.py
@@ -0,0 +1,27 @@
+import unittest
+
+import pandas as pd
+
+from src.alignment_score import compute_maximal_alignment_curve
+
+
+class TestComputeMaximalAlignmentCurve(unittest.TestCase):
+    """
+    Test functionality of mutual_clusters.compute_maximal_alignment_curve()
+    ------------
+    Example
+    ------------
+    >>> python3 -m unittest -v tests.test_compute_maximal_alignment_curve
+    """
+
+    def test_on_one_layer(self):
+        """
+        compute_maximal_alignment_curve returns a dictionary
+        """
+        _a = pd.DataFrame({"A": [0, 1, 2]})
+        _res0 = compute_maximal_alignment_curve(_a)
+        self.assertIsInstance(_res0, dict, f"""compute_maximal_alignment_curve should return a dictionary, but returned {type(_res0)}""")
+
+
+if __name__ == "__main__":
+    unittest.main()
