13ecdcab44d96d6f52277016023027414f1dd998
letiziaia <letizia852@gmail.com>
2023-02-03 18:03:42 +0200

    logging

--- src/common/logging.py
+++ src/common/logging.py
@@ -8,7 +8,7 @@ logger.configure(
             "sink": sys.stderr,
             "format": "<d>{extra}</> | "
             + "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | "
-            + "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> -"
+            + "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
             + "<level>{message}</level>",
             "serialize": False,
         }
