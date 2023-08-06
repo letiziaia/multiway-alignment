160a647af796a0024278d149c12a271b83692cf7
letiziaia <letizia852@gmail.com>
2023-08-06 18:36:23 +0300

    all full curve plot

--- src/visualizations.py
+++ src/visualizations.py
@@ -1,7 +1,11 @@
 import pandas as pd
+from joblib import load
 import matplotlib.pyplot as plt
+import matplotlib.markers as markers
+import typing
 
 from src.alignment_score import compute_maximal_alignment_curve
+from src.common.logging import logger
 
 
 def plot_maximal_alignment_curve(
@@ -47,4 +51,89 @@ def plot_maximal_alignment_curve(
     ax1.set_ylim(ymin=0)
     ax1.set_ylabel("communities sizes")
     ax1.set_xlabel("size of layers combination")
+    return fig
+
+
+def plot_full_alignment_curve(
+    cluster_labels_df: typing.Optional[pd.DataFrame],
+    which_score: typing.Optional[str],
+    adjusted: typing.Optional[bool],
+    full_dump_path: typing.Optional[str],
+) -> plt.Figure:
+    """
+    :param cluster_labels_df: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+        and column names are layers names
+    :param which_score: str, one of "nmi" or "ami" or None
+    :param adjusted: bool
+    :param full_dump_path: str
+    :return: plt.Figure
+    """
+    if full_dump_path is not None:
+        all_results = load(full_dump_path)
+    else:
+        assert (
+            cluster_labels_df is not None
+            and which_score is not None
+            and adjusted is not None
+        ), "not enough inputs"
+        all_results, _ = compute_maximal_alignment_curve(
+            cluster_labels_df=cluster_labels_df,
+            which_score=which_score,
+            adjusted=adjusted,
+        )
+
+    points = [(k.split("+")[0], v[0], k.split("+")[1:]) for k, v in all_results.items()]
+    points = pd.DataFrame(points)
+    top = points.sort_values(by=[1, 0], ascending=False).groupby(0).head(1)
+
+    logger.info(f"Area under the curve: {np.trapz(top[1], dx=1 / (len(top) - 1))}")
+
+    x = points[~points.index.isin(top.index)][0]
+    y = points[~points.index.isin(top.index)][1]
+
+    x_top = top[0]
+    y_top = top[1]
+
+    fig = plt.figure(figsize=(20, 10))
+    marker = markers.MarkerStyle(marker="s", fillstyle="none")
+    plt.plot(
+        x_top,
+        y_top,
+        marker=marker,
+        linestyle=":",
+        color="blue",
+        alpha=0.7,
+    )
+    # Annotate each point with its label
+    for i, label in top[[2]].iterrows():
+        label = label[2]
+        text = [l.replace("_", " ") for l in label]
+        text = "\n".join(text)
+        plt.annotate(
+            text,
+            (x_top[i], y_top[i]),
+            textcoords="offset points",
+            xytext=(0, 10),
+            ha="left",
+            fontsize=11,
+            # bbox=dict(boxstyle='round,pad=0.2', fc='yellow', alpha=0.5)
+        )
+    plt.scatter(
+        x,
+        y,
+        marker="x",
+        color="blueviolet",
+        alpha=0.7,
+    )
+    plt.xticks(range(2, int(max(y) + 1)))
+
+    # Remove the left and top spines
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().spines["top"].set_visible(False)
+
+    plt.xlabel("number of layers")
+    plt.ylabel("maximal alignment score")
+    plt.ylim(0, 1.0)
+    plt.tight_layout()
     return fig
