feeee39946a5738d3d6bf02af5970a0a6638dd23
letiziaia <letizia852@gmail.com>
2024-01-18 17:58:55 +0200

    refactor

---
+++ multilayer_alignment/alignment_score.py
@@ -0,0 +1,184 @@
+import typing
+import pandas as pd
+import numpy as np
+from itertools import combinations
+from functools import partial
+from joblib import dump
+
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn.metrics import adjusted_mutual_info_score
+
+import multiprocessing as mp
+from multiprocessing.pool import Pool
+from tqdm import tqdm
+
+from multilayer_alignment.mutual_clusters import compute_mutual_clusters
+from multilayer_alignment.mutual_clusters import get_mutual_clusters_labels
+
+from multilayer_alignment.utils.logging import logger
+
+
+def _compute_layer_expectation(
+    layer: np.array, scoring_function: typing.Callable
+) -> float:
+    """
+    :param layer: 1d np.array with clustering assignment
+    :param scoring_function: partial Callable, will be called on random permutations of 'layer'
+    :return: float, the expected score of 'scoring_function' under random model
+    """
+    _all_scores = []
+    with Pool(processes=mp.cpu_count() - 1) as pool:
+        result = pool.map_async(
+            scoring_function, [np.random.permutation(layer) for _ in range(10)]
+        )
+        for value in result.get():
+            # NOTE: assuming scores are always >=0
+            _all_scores.append(max(value, 0))
+    return np.array(_all_scores).mean()
+
+
+def compute_multilayer_alignment_score(
+    cluster_labels_df: pd.DataFrame,
+    mutual_clusters_labels: typing.List,
+    which_score: str = "nmi",
+    adjusted: bool = False,
+) -> float:
+    """
+    :param cluster_labels_df: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    :param mutual_clusters_labels: list, a list of labels for mutual clusters
+    :param which_score: str, one of "nmi" or "ami"
+    :param adjusted: bool, default: False
+    :return: float, between 0 and 1
+    """
+    assert which_score in ("nmi", "ami")
+
+    if which_score == "nmi":
+        _score_f = normalized_mutual_info_score
+    elif which_score == "ami":
+        _score_f = adjusted_mutual_info_score
+
+    avg_nmi = 0
+    _expected_nmi = 0
+    for layer_id in cluster_labels_df.columns:
+        _layer = cluster_labels_df[layer_id].values
+        _score = _score_f(_layer, mutual_clusters_labels, average_method="arithmetic")
+        avg_nmi += _score
+
+        if adjusted:
+            _expected_nmi += _compute_layer_expectation(
+                layer=_layer,
+                scoring_function=partial(
+                    _score_f, mutual_clusters_labels, **{"average_method": "arithmetic"}
+                ),
+            )
+    return (avg_nmi - _expected_nmi) / len(cluster_labels_df.columns)
+
+
+def compute_maximal_alignment_curve(
+    cluster_labels_df: pd.DataFrame,
+    which_score: str = "nmi",
+    adjusted: bool = False,
+    dump_to: typing.Optional[str] = None,
+) -> typing.Tuple:
+    """
+    :param cluster_labels_df: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    :param which_score: str, one of "nmi" or "ami"
+    :param adjusted: bool, default: False
+    :param dump_to: Optional[str], filename to save results
+        Default: None
+    :return: Tuple[dict[str, tuple[float, list]], dict[int, tuple[float, list, dict]]],
+        a tuple of two dictionaries, the first one including all the scores for all the combinations,
+        and the second one being the maximal alignment curve.
+        In the first dictionary, the key is the size of the combination (int) and
+        the list of layers and the value is a tuple, where the first element is the multilayer alignment score,
+        and the second element is the dictionary of mutual communities for that combination;
+        In the second dictionary, the key is the size of the combination (int) and
+        the value is a list, where the first element is the highest multilayer alignment score for that size,
+        the second element is the list of layers that gives the highest alignment score,
+        and the last element is the dictionary of mutual communities for that combination
+    """
+    assert which_score in ("nmi", "ami")
+
+    best_by_combination_size = dict()
+    all_scores_by_combination_size = dict()
+    _num_of_layers = len(cluster_labels_df.columns)
+    # skipping size 1
+    for length in range(2, _num_of_layers + 1):
+        logger.info(f"combinations of size {length}")
+        # Get all combinations of cluster_labels_df.columns of length "length"
+        _columns_combinations = combinations(cluster_labels_df.columns, length)
+
+        best_layers_combination = None
+        best_nmi = 0
+        best_layers_combination_mutual_communities = dict()
+
+        for l_comb in tqdm(_columns_combinations):
+            l_comb = list(l_comb)
+            l_comb_df = cluster_labels_df[l_comb].copy()
+            # keep only items that have labels for all items in l_comb and reindex
+            l_comb_df.dropna(inplace=True)
+            l_comb_df.reset_index(drop=True, inplace=True)
+            mutual_clusters = compute_mutual_clusters(l_comb_df)
+            mutual_clusters_labels = get_mutual_clusters_labels(mutual_clusters)
+            labels_list = (
+                mutual_clusters_labels.set_index("id")
+                .iloc[l_comb_df.index]["label"]
+                .values
+            )
+
+            # CRITERIA
+            nmi = compute_multilayer_alignment_score(
+                l_comb_df, labels_list, which_score=which_score, adjusted=adjusted
+            )
+
+            all_scores_by_combination_size[
+                f"{length}+" + "+".join(sorted(l_comb))
+            ] = nmi
+            # (
+            # nmi,
+            # mutual_clusters,
+            # )
+
+            if nmi > best_nmi:
+                best_nmi = nmi
+                best_layers_combination = l_comb
+                # best_layers_combination_mutual_communities = mutual_clusters
+
+        # RESULTS
+        best_by_combination_size[length] = (
+            best_nmi,
+            best_layers_combination,
+            # best_layers_combination_mutual_communities,
+        )
+        logger.info(
+            f"{length}-combination with highest score {best_nmi}: {best_layers_combination}"
+        )
+
+    if dump_to:
+        dump(all_scores_by_combination_size, dump_to + "_all")
+        dump(best_by_combination_size, dump_to + "_best")
+
+    return all_scores_by_combination_size, best_by_combination_size
+
+
+# def reduced_mutual_info_score(labels_true: np.array, labels_pred: np.array, **kwargs) -> float:
+#     """
+#     :param labels_true: np.array of clusters labels
+#     :param labels_pred: np.array of clusters labels
+#     :param kwargs: keywords arguments (placeholder, not used)
+#     :return: float, reduced (normalized) mutual information score
+#     """
+#     # c1 = Clustering(elm2clu_dict={i: [l] for i, l in enumerate(labels_true)})
+#     # c2 = Clustering(elm2clu_dict={i: [l] for i, l in enumerate(labels_pred)})
+#     print(labels_true)
+#     print(labels_pred)
+#     c1 = Clustering().from_membership_list(labels_true)
+#     c2 = Clustering().from_membership_list(labels_pred)
+#     bits = len(np.unique(labels_true))
+#     print(bits)
+#     print(rmi(c1, c2, norm_type="normalized", logbase=bits))
+#     print(rmi(c1, c2, norm_type="none", logbase=bits))
+#     input()
+#     return max(0, rmi(c1, c2, norm_type="normalized", logbase=bits))
---
+++ multilayer_alignment/visualizations.py
@@ -0,0 +1,286 @@
+import pandas as pd
+import numpy as np
+from joblib import load
+import matplotlib.pyplot as plt
+import matplotlib.markers as markers
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import random
+import seaborn as sns
+import typing
+
+from multilayer_alignment.alignment_score import compute_maximal_alignment_curve
+from multilayer_alignment.null_models import expected_curve_equal_sized_clusters
+from multilayer_alignment.utils.logging import logger
+
+
+def plot_maximal_alignment_curve(
+    cluster_labels_df: pd.DataFrame,
+    which_score: str = "nmi",
+    adjusted: bool = False,
+) -> plt.Figure:
+    """
+    :param cluster_labels_df: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+        and column names are layers names
+    :param which_score: str, one of "nmi" or "ami"
+    :param adjusted: bool, default: False
+    :return: plt.Figure with 2 subplots (1 row x 2 columns)
+    """
+    _, res = compute_maximal_alignment_curve(
+        cluster_labels_df=cluster_labels_df, which_score=which_score, adjusted=adjusted
+    )
+    combination_sizes = []
+    anmi_scores = []
+    communities_idx = []
+    communities_size = []
+    for key, value in res.items():
+        _anmi = value[0]
+        _mc = value[2]
+        combination_sizes.append(key)
+        anmi_scores.append(_anmi)
+        communities_idx += [key] * len(_mc)
+        communities_size += [len(v) for v in _mc.values()]
+
+    fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(21, 10))
+    ax0.plot(combination_sizes, anmi_scores, "ro--")
+    ax0.set_xticks(combination_sizes)
+    ax0.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+    ax0.set_ylim(ymin=0, ymax=1.1)
+    ax0.set_ylabel("maximal average NMI")
+    ax0.set_xlabel("size of layers combination")
+    ax1.scatter(
+        communities_idx, communities_size, s=80, facecolors="none", edgecolors="g"
+    )
+    ax1.set_xticks(combination_sizes)
+    ax1.set_yticks(range(max(communities_size) + 2, 5))
+    ax1.set_ylim(ymin=0)
+    ax1.set_ylabel("communities sizes")
+    ax1.set_xlabel("size of layers combination")
+    return fig
+
+
+def plot_full_alignment_curve(
+    cluster_labels_df: typing.Optional[pd.DataFrame],
+    which_score: typing.Optional[str],
+    adjusted: typing.Optional[bool],
+    full_dump_path: typing.Optional[str],
+) -> plt.Figure:
+    """
+    :param cluster_labels_df: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+        and column names are layers names
+    :param which_score: str, one of "nmi" or "ami" or None
+    :param adjusted: bool
+    :param full_dump_path: str
+    :return: plt.Figure
+    """
+    if full_dump_path is not None:
+        all_results = load(full_dump_path)
+    else:
+        assert (
+            cluster_labels_df is not None
+            and which_score is not None
+            and adjusted is not None
+        ), "not enough inputs"
+        all_results, _ = compute_maximal_alignment_curve(
+            cluster_labels_df=cluster_labels_df,
+            which_score=which_score,
+            adjusted=adjusted,
+        )
+
+    points = [(k.split("+")[0], v[0], k.split("+")[1:]) for k, v in all_results.items()]
+    points = pd.DataFrame(points)
+    top = points.sort_values(by=[1, 0], ascending=False).groupby(0).head(1)
+
+    logger.info(f"Area under the curve: {np.trapz(top[1], dx=1 / (len(top) - 1))}")
+
+    x = points[~points.index.isin(top.index)][0]
+    y = points[~points.index.isin(top.index)][1]
+
+    x_top = top[0]
+    y_top = top[1]
+
+    fig = plt.figure(figsize=(20, 10))
+    marker = markers.MarkerStyle(marker="s", fillstyle="none")
+    plt.plot(
+        x_top,
+        y_top,
+        marker=marker,
+        linestyle=":",
+        color="blue",
+        alpha=0.7,
+    )
+    # Annotate each point with its label
+    for i, label in top[[2]].iterrows():
+        label = label[2]
+        text = [lab.replace("_", " ") for lab in label]
+        text = "\n".join(text)
+        plt.annotate(
+            text,
+            (x_top[i], y_top[i]),
+            textcoords="offset points",
+            xytext=(0, 10),
+            ha="left",
+            fontsize=11,
+        )
+    plt.scatter(
+        x,
+        y,
+        marker="x",
+        color="blueviolet",
+        alpha=0.7,
+    )
+    plt.xticks(range(2, int(max(y) + 1)))
+
+    # Remove the left and top spines
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().spines["top"].set_visible(False)
+
+    plt.xlabel("number of layers")
+    plt.ylabel("maximal alignment score")
+    plt.ylim(0, 1.0)
+    plt.tight_layout()
+    return fig
+
+
+def plot_full_alignment_with_null_models(
+    full_result: str, full_null_path: str
+) -> plt.Figure:
+    r = load(full_result)
+    points = [(k.split("+")[0], v[0], k.split("+")[1:]) for k, v in r.items()]
+    points = pd.DataFrame(points)
+
+    points_df = []
+    for i in range(10):
+        r = load(f"{full_null_path}/null_{i}")
+        _points = [(k.split("+")[0], v[0], k.split("+")[1:]) for k, v in r.items()]
+        points_df.append(pd.DataFrame(_points))
+
+    points_df = pd.concat(points_df, ignore_index=True)
+
+    top = points.sort_values(by=[1, 0], ascending=False).groupby(0).head(1)
+    x_top = top[0]
+    x_top = [int(v) for v in x_top]
+    y_top = top[1]
+
+    _strip = points[~points.index.isin(top.index)]
+
+    null_avg = points_df.groupby(0)[1].mean().to_dict()
+    y_top_ = y_top.values - np.array([null_avg[k] for k in null_avg.keys()])
+
+    null_upper_q = points_df.groupby(0)[1].quantile(q=0.975).to_dict()
+    null_lower_q = points_df.groupby(0)[1].quantile(q=0.025).to_dict()
+    sig = [
+        (_i, v - null_avg[_i])
+        for _i, v in _strip[[0, 1]].values
+        if v > null_upper_q[_i] or v < null_lower_q[_i]
+    ]
+    not_sig = [
+        (_i, v - null_avg[_i])
+        for _i, v in _strip[[0, 1]].values
+        if null_lower_q[_i] <= v <= null_upper_q[_i]
+    ]
+
+    fig, ax = plt.subplots(1, 1, figsize=(20, 10))
+    maxim = ax.scatter(
+        x_top,
+        y_top_,
+        marker="s",
+        linestyle=":",
+        color="blue",
+        alpha=1.0,
+    )
+    # Annotate each point with its label
+    for j, (i, label) in enumerate(top[[2]].iterrows()):
+        label = label[2]
+        text = [lab.replace("_", " ") for lab in label]
+        text = "\n".join(text)
+        ax.annotate(
+            text,
+            (x_top[j], y_top_[j]),
+            textcoords="offset points",
+            xytext=(0, 10),
+            ha="left",
+            fontsize=14,
+        )
+    _sig = pd.DataFrame(sig)
+    _sig["jit"] = [
+        int(v) + (random.random() / 10) * ((-1) ** (i + 1))
+        for i, v in enumerate(_sig[0].values)
+    ]
+    _not_sig = pd.DataFrame(not_sig)
+    _not_sig["jit"] = [
+        int(v) + (random.random() / 10) * ((-1) ** (i + 1))
+        for i, v in enumerate(_not_sig[0].values)
+    ]
+    first = ax.scatter(
+        _sig["jit"], _sig[1], marker="o", color="red", alpha=0.7, label="significant"
+    )
+    second = ax.scatter(
+        _not_sig["jit"],
+        _not_sig[1],
+        marker=markers.MarkerStyle(marker="o", fillstyle="none"),
+        color="blueviolet",
+        alpha=0.7,
+        label="explained by null model",
+    )
+
+    # Remove the left and top spines
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().spines["top"].set_visible(False)
+
+    # change the fontsize
+    ax.tick_params(axis="x", labelsize=18)
+    ax.tick_params(axis="y", labelsize=18)
+
+    plt.xlabel("number of layers", fontsize=18)
+    plt.ylabel(r"$A - <A_{null}>$", fontsize=18)
+
+    # Create an inset in the lower right corner (loc=4) with borderpad=1, i.e.
+    # borderpad 1 = 10 points padding (as 10pt is the default fontsize) to the parent axes
+    axins = inset_axes(
+        ax,
+        width="35%",
+        height="35%",
+        loc=4,
+        borderpad=4,
+    )
+    axins.plot(
+        x_top,
+        y_top,
+        marker="s",
+        linestyle=":",
+        color="blue",
+        alpha=1.0,
+    )
+    sns.lineplot(
+        data=points_df,
+        x=points_df[0].astype(int),
+        y=1,
+        estimator="mean",
+        errorbar=("ci", 95),
+        n_boot=1000,
+        color="black",
+        markers="o",
+        label=r"null model (95$\%$ c.i.)",
+        ax=axins,
+    )
+    sns.lineplot(
+        x=x_top,
+        y=expected_curve_equal_sized_clusters(int(max(int(c) for c in x_top))),
+        linestyle="-.",
+        color="grey",
+        label="equal-sized clusters",
+    )
+    axins.set_xlabel("number of layers")
+    axins.set_ylabel(r"$A$")
+    axins.set_ylim(0, 1.0)
+    axins.set_aspect(1.0 / axins.get_data_ratio(), adjustable="box")
+
+    ax.legend(
+        (maxim, first, second),
+        ("maximum", "significant", "explained by null model"),
+        loc="center right",
+        fontsize="12",
+    )
+    return fig
