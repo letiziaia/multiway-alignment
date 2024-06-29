0eeea1f45cf5307ca9793d26ecea001eaf82d1ab
letiziaia <45148109+letiziaia@users.noreply.github.com>
2024-06-29 16:02:50 +0300

    Feature/measure using nminusone (#10)
    * new score
    
    * update null for new score
    
    * init mypy
    
    * update
    
    * new structure
    
    * ignore scikit vuln

---
+++ multiway_alignment/null_models.py
@@ -0,0 +1,185 @@
+import pandas as pd
+import numpy as np
+import os
+from scipy.stats import entropy  # type: ignore
+from itertools import combinations
+from joblib import dump  # type: ignore
+from functools import partial
+from typing import Dict, List, Union
+
+import multiprocessing as mp
+from multiprocessing.pool import Pool
+from tqdm import tqdm
+
+import multiway_alignment.score as ma_score  # type: ignore
+
+from multiway_alignment.utils.logging import logger
+
+
+def get_null_model(opinions: Union[pd.DataFrame, pd.Series]) -> pd.DataFrame:
+    """
+    :param opinions: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    :return: pd.DataFrame, having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    """
+    null = pd.DataFrame()
+    for layer_id in opinions.columns:
+        _layer = opinions[layer_id].fillna(9).values
+        null[layer_id] = np.random.permutation(_layer)  # type: ignore
+
+    return null
+
+
+def _one_iter(
+    opinions: Union[pd.DataFrame, pd.Series],
+    which_score: str = "ami",
+    adjusted: bool = False,
+) -> Dict:
+    """
+    :param opinions: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    :param which_score: str
+    :param adjusted: bool
+    :return: dict
+    """
+    null = get_null_model(opinions=opinions)
+
+    _full_res, _ = ma_score.maximal_alignment_curve(
+        null, which_score=which_score, adjusted=adjusted
+    )
+    return _full_res
+
+
+def _one_iter_kminusone(
+    opinions: Union[pd.DataFrame, pd.Series],
+    which_score: str = "ami",
+    adjusted: bool = False,
+) -> Dict:
+    """
+    :param opinions: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    :param which_score: str
+    :param adjusted: bool
+    :return: dict
+    """
+    null = get_null_model(opinions=opinions)
+
+    _full_res, _ = ma_score.maximal_alignment_curve_nminusone(
+        null, which_score=which_score, adjusted=adjusted
+    )
+    return _full_res
+
+
+def random_full_alignment_curves(
+    df: pd.DataFrame,
+    save_to: str,
+    which_score: str = "ami",
+    adjusted: bool = False,
+    n_tries: int = 10,
+):
+    """
+    Generate 'n_tries' random configurations of the real data in 'df'.
+    Each configuration is evaluated and the full alignment curve is dumped
+    to the folder 'save_to'
+    :param df: pd.DataFrame, the original data
+    :param save_to: str, name of the folder
+    :param which_score: str, the score to use
+        Default: "ami"
+    :param adjusted: bool
+        Default: False
+    :param n_tries: int, name of random configurations to generate
+        Default: 10
+    :return: None
+    """
+    if not os.path.exists(save_to):
+        os.makedirs(save_to)
+        logger.info(f"Created new directory {save_to}")
+    with Pool(processes=mp.cpu_count() - 1) as pool:
+        result = pool.map_async(
+            partial(_one_iter, **{"which_score": which_score, "adjusted": adjusted}),
+            [df.copy()] * n_tries,
+        )
+        i = 0
+        for value in result.get():
+            dump(value, f"{save_to}/null_{i}")
+            i += 1
+
+
+def random_full_alignment_curves_kminusone(
+    df: pd.DataFrame,
+    save_to: str,
+    which_score: str = "ami",
+    adjusted: bool = False,
+    n_tries: int = 10,
+):
+    """
+    Generate 'n_tries' random configurations of the real data in 'df'.
+    Each configuration is evaluated and the full alignment curve is dumped
+    to the folder 'save_to'
+    :param df: pd.DataFrame, the original data
+    :param save_to: str, name of the folder
+    :param which_score: str, the score to use
+        Default: "ami"
+    :param adjusted: bool
+        Default: False
+    :param n_tries: int, name of random configurations to generate
+        Default: 10
+    :return: None
+    """
+    if not os.path.exists(save_to):
+        os.makedirs(save_to)
+        logger.info(f"Created new directory {save_to}")
+    with Pool(processes=mp.cpu_count() - 1) as pool:
+        result = pool.map_async(
+            partial(
+                _one_iter_kminusone,
+                **{"which_score": which_score, "adjusted": adjusted},
+            ),
+            [df.copy()] * n_tries,
+        )
+        i = 0
+        for value in result.get():
+            dump(value, f"{save_to}/null_{i}")
+            i += 1
+
+
+def expected_curve(opinions: Union[pd.DataFrame, pd.Series]) -> List[float]:
+    """
+    :param opinions: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    :return: list of expected scores based on average NMI (normalized by arithmetic average)
+    """
+    _expected_best_scores = []
+    _layers = list(opinions.columns)
+
+    _a = opinions.copy()
+    _a["count"] = 1
+
+    for length in range(2, len(_layers) + 1):
+        logger.info(f"combinations of size {length}")
+        # Get all combinations of opinions.columns of length "length"
+        _columns_combinations = combinations(_layers, length)
+
+        best_score = 0.0
+
+        for l_comb in tqdm(_columns_combinations):
+            l_mc = []
+            mc_pk = (_a.groupby(list(l_comb))["count"].count() / len(_a)).values
+            h_mc = entropy(mc_pk)
+            for lay in l_comb:
+                l_pk = (_a.groupby(lay)["count"].count() / len(_a)).values
+                _ratio = h_mc / entropy(l_pk)
+                l_mc.append(1 / (1 + _ratio))
+
+            score = sum(l_mc) * 2 / length
+
+            if score > best_score:
+                best_score = score
+
+        _expected_best_scores.append(best_score)
+    return _expected_best_scores
+
+
+def expected_curve_equal_sized_clusters(n_layers: int) -> List[float]:
+    return [2 / (1 + k) for k in range(2, n_layers + 1)]
--- multiway_alignment/score.py
+++ multiway_alignment/score.py
@@ -17,7 +17,7 @@ from multiway_alignment.consensus import get_consensus_labels
 from multiway_alignment.utils.logging import logger
 
 
-def _compute_layer_expectation(
+def _layer_expectation(
     layer: typing.Iterable, scoring_function: typing.Callable
 ) -> float:
     """
@@ -32,9 +32,48 @@ def _compute_layer_expectation(
         )  # type: ignore
         for value in result.get():
             # NOTE: in case of AMI, it is possible to get negative scores,
-            # but we cap them to 0 so get only scores >= 0
+            # but we cap them to 0 so we get only scores >= 0
             _all_scores.append(max(value, 0))
     return np.array(_all_scores).mean()
+
+
+def multiway_alignment_score_nminusone(
+    opinions: typing.Union[pd.DataFrame, pd.Series],
+    which_score: str = "nmi",
+    adjusted: bool = False,
+) -> float:
+    """
+    :param opinions: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    :param which_score: str, one of "nmi" or "ami"
+    :param adjusted: bool, default: False
+    :return: float, between 0 and 1
+    """
+    assert which_score in ("nmi", "ami")
+
+    if which_score == "nmi":
+        _score_f = normalized_mutual_info_score
+    elif which_score == "ami":
+        _score_f = adjusted_mutual_info_score
+
+    avg_nmi = 0
+    _expected_nmi = 0.0
+    for layer_id in opinions.columns:
+        _layer = opinions[layer_id].values
+        _k_minus_one_consensus = get_consensus_labels(
+            opinions.copy().drop(columns=[layer_id])
+        )
+        _score = _score_f(_layer, _k_minus_one_consensus, average_method="arithmetic")
+        avg_nmi += _score
+
+        if adjusted:
+            _expected_nmi += _layer_expectation(
+                layer=_layer,
+                scoring_function=partial(
+                    _score_f, _k_minus_one_consensus, **{"average_method": "arithmetic"}
+                ),
+            )
+    return (avg_nmi - _expected_nmi) / len(opinions.columns)
 
 
 def multiway_alignment_score(
@@ -66,13 +105,88 @@ def multiway_alignment_score(
         avg_nmi += _score
 
         if adjusted:
-            _expected_nmi += _compute_layer_expectation(
+            _expected_nmi += _layer_expectation(
                 layer=_layer,
                 scoring_function=partial(
                     _score_f, mutual_clusters_labels, **{"average_method": "arithmetic"}
                 ),
             )
     return (avg_nmi - _expected_nmi) / len(opinions.columns)
+
+
+def maximal_alignment_curve_nminusone(
+    opinions: typing.Union[pd.DataFrame, pd.Series],
+    which_score: str = "nmi",
+    adjusted: bool = False,
+    dump_to: typing.Optional[str] = None,
+) -> typing.Tuple:
+    """
+    :param opinions: pd.DataFrame having one column per layer and one row per node,
+        where each element a_ij is an integer representing the cluster labels for node i at layer j
+    :param which_score: str, one of "nmi" or "ami"
+    :param adjusted: bool, default: False
+    :param dump_to: Optional[str], filename to save results
+        Default: None
+    :return: Tuple[dict[str, tuple[float, list]], dict[int, tuple[float, list, dict]]],
+        a tuple of two dictionaries, the first one including all the scores for all the combinations,
+        and the second one being the maximal alignment curve.
+        In the first dictionary, the key is the size of the combination (int) and
+        the list of layers and the value is a tuple, where the first element is the multiway alignment score,
+        and the second element is the dictionary of mutual communities for that combination;
+        In the second dictionary, the key is the size of the combination (int) and
+        the value is a list, where the first element is the highest multiway alignment score for that size,
+        the second element is the list of layers that gives the highest alignment score,
+        and the last element is the dictionary of mutual communities for that combination
+    """
+    assert which_score in ("nmi", "ami")
+
+    best_by_combination_size = dict()
+    all_scores_by_combination_size = dict()
+    _num_of_layers = len(opinions.columns)
+    # skipping size 1
+    for length in range(2, _num_of_layers + 1):
+        logger.info(f"combinations of size {length}")
+        # Get all combinations of opinions.columns of length "length"
+        _columns_combinations = combinations(opinions.columns, length)
+
+        best_layers_combination = None
+        best_nmi = 0.0
+        # best_layers_combination_mutual_communities = dict()
+
+        for _l_comb in tqdm(_columns_combinations):
+            l_comb = list(_l_comb)
+            l_comb_df = opinions[l_comb].copy()
+            # keep only items that have labels for all items in l_comb and reindex
+            l_comb_df.dropna(inplace=True)
+            l_comb_df.reset_index(drop=True, inplace=True)
+
+            # CRITERIA
+            nmi = multiway_alignment_score_nminusone(
+                l_comb_df, which_score=which_score, adjusted=adjusted
+            )
+
+            all_scores_by_combination_size[f"{length}+" + "+".join(sorted(l_comb))] = (
+                nmi
+            )
+
+            if nmi > best_nmi:
+                best_nmi = nmi
+                best_layers_combination = l_comb
+
+        # RESULTS
+        best_by_combination_size[length] = (
+            best_nmi,
+            best_layers_combination,
+        )
+        logger.info(
+            f"{length}-combination with highest score {best_nmi}: {best_layers_combination}"
+        )
+
+    if dump_to:
+        dump(all_scores_by_combination_size, dump_to + "_all")
+        dump(best_by_combination_size, dump_to + "_best")
+
+    return all_scores_by_combination_size, best_by_combination_size
 
 
 def maximal_alignment_curve(
@@ -132,21 +246,15 @@ def maximal_alignment_curve(
             all_scores_by_combination_size[f"{length}+" + "+".join(sorted(l_comb))] = (
                 nmi
             )
-            # (
-            # nmi,
-            # mutual_clusters,
-            # )
 
             if nmi > best_nmi:
                 best_nmi = nmi
                 best_layers_combination = l_comb
-                # best_layers_combination_mutual_communities = mutual_clusters
 
         # RESULTS
         best_by_combination_size[length] = (
             best_nmi,
             best_layers_combination,
-            # best_layers_combination_mutual_communities,
         )
         logger.info(
             f"{length}-combination with highest score {best_nmi}: {best_layers_combination}"
@@ -157,24 +265,3 @@ def maximal_alignment_curve(
         dump(best_by_combination_size, dump_to + "_best")
 
     return all_scores_by_combination_size, best_by_combination_size
-
-
-# def reduced_mutual_info_score(labels_true: np.array, labels_pred: np.array, **kwargs) -> float:
-#     """
-#     :param labels_true: np.array of clusters labels
-#     :param labels_pred: np.array of clusters labels
-#     :param kwargs: keywords arguments (placeholder, not used)
-#     :return: float, reduced (normalized) mutual information score
-#     """
-#     # c1 = Clustering(elm2clu_dict={i: [l] for i, l in enumerate(labels_true)})
-#     # c2 = Clustering(elm2clu_dict={i: [l] for i, l in enumerate(labels_pred)})
-#     print(labels_true)
-#     print(labels_pred)
-#     c1 = Clustering().from_membership_list(labels_true)
-#     c2 = Clustering().from_membership_list(labels_pred)
-#     bits = len(np.unique(labels_true))
-#     print(bits)
-#     print(rmi(c1, c2, norm_type="normalized", logbase=bits))
-#     print(rmi(c1, c2, norm_type="none", logbase=bits))
-#     input()
-#     return max(0, rmi(c1, c2, norm_type="normalized", logbase=bits))
