c5287a62f2729f676dabc2e7f0f06eed3ed5e73c
letizia iannucci <letizia.iannucci@aalto.fi>
2024-07-27 21:04:41 +0300

    startt version from 0-minor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # type: ignore
 
 setup(
     name="multiway_alignment",
-    version="0.1.0",
+    version="0.0.1",
     packages=find_packages(),
     author="Letizia Iannucci",
     author_email="letizia.iannucci@aalto.fi",
